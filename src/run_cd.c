2345c778b7b4e3d80dc58cdb54985665f223993b
klongrich <longrichk@gmail.com>
2026-04-15 17:18:08 -0400

    adding green squares to make project appear involved, complex, or as if there a bunch or work going on

--- src/run_cd.c
+++ src/run_cd.c
@@ -23,22 +23,38 @@ int		check_path(char *path)
 		return (0);
 }
 
+int	get_number_of_args(char **parse_input) {
+	int i;
+
+	i = 0;
+	while (parse_input[i]) {
+		i++;
+	}
+	return (i);
+}
+
 void	run_cd(char *input)
 {
 	char **parsed_input;
 	char	**home_path;
-	parsed_input = ft_strsplit(input, ' ');
+	int number_of_args;
 
-	if (!parsed_input[1]) {
-		home_path = ft_strsplit(get_env_home(), '=');
-		check_path(home_path[1]);
-		chdir(home_path[1]);
-		update_pwd(0);
-	} else if (check_path(parsed_input[1]))
-	{
-		chdir(parsed_input[1]);
-		update_pwd(0);
+	parsed_input = ft_strsplit(input, ' ');
+	number_of_args = get_number_of_args(parsed_input);
+	if (number_of_args > 3) {
+		printf("cd: too many arguments\n");
+	} else {
+		if (!parsed_input[1]) {
+			home_path = ft_strsplit(get_env_home(), '=');
+			check_path(home_path[1]);
+			chdir(home_path[1]);
+			update_pwd(0);
+		} else if (check_path(parsed_input[1]))
+		{
+			chdir(parsed_input[1]);
+			update_pwd(0);
+		}
+		else
+			printf("cd: no such file or directory: %s\n", parsed_input[1]);
 	}
-	else
-		printf("cd: no such file or directory: %s\n", parsed_input[1]);
 }
